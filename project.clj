db386043b9b51e00d4c1672c85364bb9046940b6
Enzo Vergara <vergara.enzo@gmail.com>
2018-10-20 23:15:23 +0800

    add url to project

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject
   coins "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://t.me/coinsphalerts"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
